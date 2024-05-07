9af5199cb96f963a384c8e6d28a3246f50f49376
Vivek-02 <56870780+Vivek-02@users.noreply.github.com>
2024-05-07 23:38:47 +0530

    adding group by stream feature example

--- src/functional/MainClass.java
+++ src/functional/MainClass.java
@@ -1,18 +1,47 @@
 package functional;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class MainClass {
 
     public static void main(String[] args) {
         // Testing functional interface
-        int a = 5;
-        Square s = (int x)->x*x;
-        int ans = s.area(a);
-        System.out.println(ans);
+        MainClass mainClass = new MainClass();
+        int[] people = {3,2,2,1};
+        System.out.println(mainClass.numRescueBoats(people,3));
+    }
 
-        // Testing Consumer FI
-        Consumer<Integer> consumer = (value) -> System.out.println(value);
-        consumer.accept(5);
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int sum = 0;
+        int i = 0;
+        int j = people.length-1;
+        int count = 0;
+        Set<Integer> set = new HashSet<>();
+
+        while(i < j) {
+            sum = people[i] + people[j];
+            if(sum<=limit) {
+                sum=0;
+                count++;
+                set.add(i);
+                set.add(j);
+                i++;
+                j--;
+            } else if(sum>limit) {
+                sum = 0;
+                j--;
+            }
+        }
+
+        for(int k=i;k<people.length;k++) {
+            if(!set.contains(k))
+                count++;
+        }
+
+        return count;
     }
 }
--- src/streams/StreamFeatures.java
+++ src/streams/StreamFeatures.java
@@ -1,14 +1,42 @@
 package streams;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamFeatures {
 
     public static void main(String[] args) {
+        StreamFeatures streamFeatures = new StreamFeatures();
+        streamFeatures.testStreamsGroupBy();
+    }
+
+    private void testStreamsGroupBy() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Jordan","A+");
+        map.put("Messi","B+");
+        map.put("Ronaldo","O+");
+        map.put("Maradona","A+");
+        map.put("Dhoni","B+");
+        map.put("Virat","A+");
+        map.put("Siraj","B+");
+        map.put("Sami","O+");
+        map.put("Pujara","O+");
+        map.put("Agarkar","A+");
+
+        Map<String,List<String>> groupByBloodGroupMap = map.keySet().stream()
+                .collect(Collectors.groupingBy(map::get));
+
+        System.out.println(groupByBloodGroupMap);
+    }
+
+    public void testSampleMapAndPredicate() {
         List<Integer> list = Arrays.asList(1,2,3,4,5);
         List<Integer> square = list.stream().map(x->x*x).collect(Collectors.toList());
+        System.out.println(square);
 
         List<String> names = Arrays.asList("Vivek","Verma","Bhavana","Gutta");
         List<String> filtered = names.stream().filter(s -> s.startsWith("V")).collect(Collectors.toList());
