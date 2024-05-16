4bfd3d4e713b0392213c234be66200b6977f0b29
Vivek-02 <56870780+Vivek-02@users.noreply.github.com>
2024-05-16 22:16:20 +0530

    adding flat map example

--- src/streams/FlatMapTest.java
+++ src/streams/FlatMapTest.java
@@ -5,10 +5,15 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FlatMapTest {
 
     public static void main(String[] args) {
+        FlatMapTest.flattenStringListExample();
+    }
+
+    public static void flattenIntegerListExample() {
         List<Integer> list1 = List.of(1);
         List<Integer> list2 = Arrays.asList(2,3);
         List<Integer> list3 = Arrays.asList(4,5,6);
@@ -22,5 +27,11 @@ public class FlatMapTest {
         List<Integer> derived = main.stream().flatMap(Collection::stream).collect(Collectors.toList());
 
         System.out.println("After -> " + derived);
+    }
+
+    public static void flattenStringListExample() {
+        List<String> list = Arrays.asList("John", "Wick", "Jason", "Bourne", "James", "Bond");
+
+        list.stream().flatMap(s -> Stream.of(s.charAt(2))).forEach(System.out::print);
     }
 }
